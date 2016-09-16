a602cb4478178cf7c78b594e0d2347160eea56ea
Jamie Olsen <jwolsen@mmm.com>
2016-09-16 14:27:50 -0600

    Created Utility class and got rid of MovieContract enum

--- PopularMovies/app/src/main/java/com/example/jamie/popularmovies/Utility.java
+++ PopularMovies/app/src/main/java/com/example/jamie/popularmovies/Utility.java
@@ -5,5 +5,5 @@ package com.example.jamie.popularmovies;
  */
 public class Utility {
     public static String MOVIE_KEY = "movie";
-    public static String MOVIE_API_KEY = "02a6d79992ed3e3da1f638dec4c74770";
+    public static String MOVIE_API_KEY = "<ENTER KEY HERE>";
 }

7d3fe46b06b9ae7eacb5d93866e424ed28b409b4
jolsendev <jolsendev@gmail.com>
2016-12-01 23:53:26 -0700

    Added Stetho... cool...

--- PopularMovies/app/src/main/java/com/example/jamie/popularmovies/MainActivity.java
+++ PopularMovies/app/src/main/java/com/example/jamie/popularmovies/MainActivity.java
@@ -7,6 +7,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.facebook.stetho.DumperPluginsProvider;
+import com.facebook.stetho.Stetho;
+import com.facebook.stetho.dumpapp.DumpException;
+import com.facebook.stetho.dumpapp.DumperContext;
+import com.facebook.stetho.dumpapp.DumperPlugin;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +28,21 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        new OkHttpClient.Builder()
+                .addNetworkInterceptor(new StethoInterceptor())
+                .build();
+        Stetho.initialize(Stetho.newInitializerBuilder(this)
+                .enableDumpapp(new DumperPluginsProvider() {
+                    @Override
+                    public Iterable<DumperPlugin> get() {
+                        return new Stetho.DefaultDumperPluginsBuilder(getApplicationContext())
+                                .provide(new MyDumperPlugin())
+                                .finish();
+                    }
+                })
+                .enableWebKitInspector(Stetho.defaultInspectorModulesProvider(this))
+                .build());
 
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
@@ -44,5 +73,38 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private class MyDumperPlugin implements DumperPlugin {
+        @Override
+        public String getName() {
+            return null;
+        }
+
+        @Override
+        public void dump(DumperContext dumpContext) throws DumpException {
+
+        }
+    }
+
+    private class StethoInterceptor implements Interceptor {
+        @Override
+        public Response intercept(Chain chain) throws IOException {
+            //I found thid code here: https://github.com/square/okhttp/wiki/Interceptors
+            Request request = chain.request();
+
+            long t1 = System.nanoTime();
+            Logger logger = null;
+            logger.info(String.format("Sending request %s on %s%n%s",
+                    request.url(), chain.connection(), request.headers()));
+
+            Response response = chain.proceed(request);
+
+            long t2 = System.nanoTime();
+            logger.info(String.format("Received response for %s in %.1fms%n%s",
+                    response.request().url(), (t2 - t1) / 1e6d, response.headers()));
+
+            return response;
+        }
     }
 }
